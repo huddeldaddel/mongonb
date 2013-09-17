216bcf5279cd1622df8ff6db3c80bafbf506f8d9
thomaswerner35 <thomaswerner35@e4a4c797-1e2a-40b8-b7b3-b58e2255469f>
2013-09-17 19:09:35 +0000

    Worked on Node of the Services window: - Added DatabaseNode - Added Actions for connect and disconnect - Minor changes

--- ui-core/src/de/bfg9000/mongonb/ui/core/services/ConnectionNode.java
+++ ui-core/src/de/bfg9000/mongonb/ui/core/services/ConnectionNode.java
@@ -1,6 +1,8 @@
 package de.bfg9000.mongonb.ui.core.services;
 
 import de.bfg9000.mongonb.core.Connection;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javax.swing.Action;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
@@ -10,7 +12,7 @@ import org.openide.nodes.Children;
  * 
  * @author wernert
  */
-class ConnectionNode extends AbstractNode {
+class ConnectionNode extends AbstractNode implements PropertyChangeListener {
     
     public static final String ICON_CONNECTED = "de/bfg9000/mongonb/ui/core/images/connected.png";   
     public static final String ICON_DISCONNECTED = "de/bfg9000/mongonb/ui/core/images/disconnected.png";   
@@ -18,20 +20,35 @@ class ConnectionNode extends AbstractNode {
     private final Connection connection;
     
     public ConnectionNode(Connection connection) {
-        super(Children.LEAF);
-        
+        super(Children.create(new DatabaseNodeFactory(connection), true));        
         this.connection = connection;
+        this.connection.addPropertyChangeListener(this);
         
         setName(connection.getName());
         setIconBaseWithExtension(connection.isConnected() ? ICON_CONNECTED : ICON_DISCONNECTED);        
     }
     
     @Override
+    public Action getPreferredAction() {
+        return connection.isConnected() ? null : new ConnectAction(connection);
+    }
+    
+    @Override
     public Action[] getActions(boolean context) {
         return new Action[] { 
+            new ConnectAction(connection),
+            new DisconnectAction(connection),
             new DeleteConnectionAction(connection),
             new EditConnectionAction(connection)
         };        
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(evt.getPropertyName().equals(Connection.PROPERTY_CONNECTED)) {
+            setIconBaseWithExtension(Boolean.TRUE.equals(evt.getNewValue()) ? ICON_CONNECTED : ICON_DISCONNECTED);
+            fireIconChange();
+        }
     }
     
 }

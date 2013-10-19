93231adb8f18d2c08a1d286904d36b61c6a80056
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2013-10-19 08:58:23 +0000

    - Restructured the project: removed the suite and its subprojects and merged all contents into a single NetBeans Module project

---
+++ src/de/bfg9000/mongonb/Installer.java
@@ -0,0 +1,27 @@
+package de.bfg9000.mongonb;
+
+import de.bfg9000.mongonb.core.Connection;
+import de.bfg9000.mongonb.core.ConnectionManager;
+import static com.jidesoft.utils.Lm.*;
+import org.openide.modules.ModuleInstall;
+
+/**
+ * Initializes the JIDE components with the Open Source license.
+ * 
+ * @author wernert
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public boolean closing() {
+        for(Connection con: ConnectionManager.INSTANCE.getConnections())
+            con.disconnect();
+        return super.closing();        
+    }
+    
+    @Override
+    public void restored() {
+        verifyLicense("Thomas Werner", "MongoNb", "7okEKvBubaMAOOroAD0ROAUoGaWMu0L1");
+    }
+    
+}

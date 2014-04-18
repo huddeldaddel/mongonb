fed34954e0936957c58af1b35a351ad944107108
thomaswerner35 <thomaswerner35@e4a4c797-1e2a-40b8-b7b3-b58e2255469f>
2014-04-18 11:03:31 +0000

    Worked on Collection class: - Added a method that returns all fields that are used by elements of a given collection - Added JavaDoc for methods

--- src/de/bfg9000/mongonb/core/Collection.java
+++ src/de/bfg9000/mongonb/core/Collection.java
@@ -9,8 +9,10 @@ import static com.mongodb.MapReduceCommand.OutputType.INLINE;
 import com.mongodb.MapReduceOutput;
 import com.mongodb.WriteResult;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -26,6 +28,12 @@ public class Collection {
     @Getter private final String database;
     @Getter private final String name;
 
+    /**
+     * Stores the given document into this collection
+     * @param document the document to be stored, as JSON
+     * @return the DBObject that has been written
+     * @throws IOException in case of error
+     */
     public DBObject add(String document) throws IOException {
         final DBObject dbObject = new QueryObjectConverter().convertQueryObject(document);
         final WriteResult result = connection.getMongoClient().getDB(database).getCollection(name).insert(dbObject);
@@ -35,18 +43,38 @@ public class Collection {
         return dbObject;
     }
 
+    /**
+     * Performs a map/reduce operation on this collection.
+     * @param mapFunction the map function to be applied on the entries of the collection
+     * @param reduceFunction the reduce function to be applied on the result of the map function
+     * @param executor the executor to be used
+     * @return the QueryResult
+     */
     public QueryResult mapReduce(String mapFunction, String reduceFunction, QueryExecutor executor) {
         final DBCollection collection = connection.getMongoClient().getDB(database).getCollection(name);
-        final MapReduceOutput out = collection.mapReduce(mapFunction, reduceFunction, null, INLINE, new BasicDBObject());
+        final MapReduceOutput out= collection.mapReduce(mapFunction, reduceFunction, null, INLINE, new BasicDBObject());
         return new QueryResult.MapReduceResult(out, executor);
     }
 
+    /**
+     * Performs a query operation on this collection.
+     * @param query the template object
+     * @param executor the executor to be used
+     * @return the QueryResult
+     * @throws IOException in case of error
+     */
     public QueryResult query(String query, QueryExecutor executor) throws IOException {
         final DBObject queryObject = new QueryObjectConverter().convertQueryObject(query);
         final DBCursor cursor = connection.getMongoClient().getDB(database).getCollection(name).find(queryObject);
         return new QueryResult.DBCursorResult(cursor, executor);
     }
 
+    /**
+     * Performs a remove operation on this collection.
+     * @param query the template object
+     * @return the documents that have been removed
+     * @throws IOException in case of error
+     */
     public List<DBObject> remove(String query) throws IOException {
         final List<DBObject> result = new LinkedList<DBObject>();
         final DBObject queryObject = new QueryObjectConverter().convertQueryObject(query);
@@ -60,8 +88,37 @@ public class Collection {
         return result;
     }
 
+    /**
+     * Drops the collection. All data will be lost.
+     */
     public void drop() {
         connection.getMongoClient().getDB(database).getCollection(name).drop();
+    }
+
+    /**
+     * Gets the fields that are used by the documents in this collection. This is a rather lengthy operation because all
+     * elements have to be analyzed (by a map/reduce query).
+     *
+     * @return a map of (field name)-&gt;(# of usages)
+     */
+    public Map<String, Integer> getFields() {
+        final String mapFunction = "" +
+                "function() {                         \n" +
+                "   for(var prop in this)             \n" +
+                "       if(this.hasOwnProperty(prop)) \n" +
+                "           emit(prop, 1);            \n" +
+                "}";
+        final String reduceFunction = "" +
+                "function(key, values) {    \n" +
+                "    return values.length;  \n" +
+                "}";
+        final DBCollection collection = connection.getMongoClient().getDB(database).getCollection(name);
+        final MapReduceOutput out= collection.mapReduce(mapFunction, reduceFunction, null, INLINE, new BasicDBObject());
+
+        final Map<String, Integer> result = new HashMap<String, Integer>();
+        for(DBObject object: out.results())
+            result.put((String)object.get("_id"), ((Double)object.get("value")).intValue());
+        return result;
     }
 
 }

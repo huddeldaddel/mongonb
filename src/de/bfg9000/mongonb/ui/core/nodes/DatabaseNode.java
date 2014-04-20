bf0a7652782eaf39a0142561bb7ee9cb530390b9
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2014-04-20 12:00:48 +0000

    Worked on Node classes: - Added default action to context menu - Added properties to DatabaseNode and ConnectionNode

--- src/de/bfg9000/mongonb/ui/core/nodes/DatabaseNode.java
+++ src/de/bfg9000/mongonb/ui/core/nodes/DatabaseNode.java
@@ -1,10 +1,21 @@
 package de.bfg9000.mongonb.ui.core.nodes;
 
+import com.mongodb.CommandResult;
+import com.mongodb.DBObject;
 import de.bfg9000.mongonb.core.Database;
 import de.bfg9000.mongonb.ui.core.actions.CreateCollectionAction;
+import java.lang.reflect.InvocationTargetException;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
 import javax.swing.Action;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.PropertySupport;
+import org.openide.nodes.Sheet;
+import org.openide.util.NbBundle;
 
 /**
  * Displays a {@code Database} in the Services window.
@@ -14,6 +25,8 @@ import org.openide.nodes.Children;
 class DatabaseNode extends AbstractNode {
 
     public static final String ICON = "de/bfg9000/mongonb/ui/core/images/database.png";
+
+    private static final ResourceBundle bundle = NbBundle.getBundle(DatabaseNode.class);
 
     private final Database database;
 
@@ -27,9 +40,67 @@ class DatabaseNode extends AbstractNode {
 
     @Override
     public Action[] getActions(boolean context) {
-        return new Action[] {
-            new CreateCollectionAction(database)
-        };
+        final List<Action> actions = new LinkedList<Action>();
+        actions.add(new CreateCollectionAction(database));
+        actions.addAll(Arrays.asList(super.getActions(context)));
+        return actions.toArray(new Action[actions.size()]);
+    }
+
+    @Override
+    protected Sheet createSheet() {
+        final Sheet result = new Sheet();
+        final Sheet.Set defaultProperties = Sheet.createPropertiesSet();
+        final CommandResult stats = database.getStats();
+        defaultProperties.put(new LocalizedProperty("serverUsed", stats.get("serverUsed")));
+        defaultProperties.put(new LocalizedProperty("db", stats.get("db")));
+        defaultProperties.put(new LocalizedProperty("collections", stats.get("collections") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("collections")) : ""));
+        defaultProperties.put(new LocalizedProperty("objects", stats.get("objects") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("objects")) : ""));
+        defaultProperties.put(new LocalizedProperty("avgObjSize", stats.get("avgObjSize") instanceof Double ?
+                              NumberFormat.getNumberInstance().format((Double) stats.get("avgObjSize")) : ""));
+        defaultProperties.put(new LocalizedProperty("dataSize", stats.get("dataSize") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("dataSize")) : ""));
+        defaultProperties.put(new LocalizedProperty("storageSize", stats.get("storageSize") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("storageSize")) : ""));
+        defaultProperties.put(new LocalizedProperty("numExtents", stats.get("numExtents") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("numExtents")) : ""));
+        defaultProperties.put(new LocalizedProperty("indexes", stats.get("indexes") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("indexes")) : ""));
+        defaultProperties.put(new LocalizedProperty("indexSize", stats.get("indexSize") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("indexSize")) : ""));
+        defaultProperties.put(new LocalizedProperty("fileSize", stats.get("fileSize") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("fileSize")) : ""));
+        defaultProperties.put(new LocalizedProperty("nsSizeMB", stats.get("nsSizeMB") instanceof Integer ?
+                              NumberFormat.getIntegerInstance().format((Integer) stats.get("nsSizeMB")) : ""));
+        defaultProperties.put(new LocalizedProperty("dataFileVersion", stats.get("dataFileVersion") instanceof DBObject?
+                              ((DBObject)stats.get("dataFileVersion")).get("major") +"." +
+                              ((DBObject)stats.get("dataFileVersion")).get("minor") : ""));
+        defaultProperties.put(new LocalizedProperty("ok", Double.valueOf(1.0).equals(stats.get("ok")) ?
+                              bundle.getString("DatabaseNode.property.value.yes") :
+                              bundle.getString("DatabaseNode.property.value.no")));
+        result.put(defaultProperties);
+        return result;
+    }
+
+    /**
+     * Read-Only property that uses localized strings from the bundle file.
+     */
+    private final static class LocalizedProperty extends PropertySupport.ReadOnly<String> {
+
+        private final Object data;
+
+        public LocalizedProperty(String propertyName, Object data) {
+            super(bundle.getString("DatabaseNode.property." +propertyName +".name"), String.class,
+                  bundle.getString("DatabaseNode.property." +propertyName +".displayname"),
+                  bundle.getString("DatabaseNode.property." +propertyName +".shortdesc"));
+            this.data = data;
+        }
+
+        @Override
+        public String getValue() throws IllegalAccessException, InvocationTargetException {
+            return null == data ? "" : data.toString();
+        }
     }
 
 }
--- src/de/bfg9000/mongonb/ui/core/nodes/MongoServiceNode.java
+++ src/de/bfg9000/mongonb/ui/core/nodes/MongoServiceNode.java
@@ -1,6 +1,9 @@
 package de.bfg9000.mongonb.ui.core.nodes;
 
 import de.bfg9000.mongonb.ui.core.actions.CreateConnectionAction;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javax.swing.Action;
 import org.netbeans.api.core.ide.ServicesTabNodeRegistration;
@@ -10,32 +13,35 @@ import org.openide.util.NbBundle;
 
 /**
  * This Node is the base node of the plugin. It is registered as a root entry of the Services tab.
- * 
+ *
  * @author thomaswerner35
  */
 @ServicesTabNodeRegistration(
-        displayName=MongoServiceNode.NAME, 
-        iconResource=MongoServiceNode.ICON_BASE, 
-        name=MongoServiceNode.NAME, 
+        displayName=MongoServiceNode.NAME,
+        iconResource=MongoServiceNode.ICON_BASE,
+        name=MongoServiceNode.NAME,
         position = 5000
 )
 public class MongoServiceNode extends AbstractNode {
-    
-    public static final String ICON_BASE = "de/bfg9000/mongonb/ui/core/images/MongoDB-logo-16.png";    
-    public static final String NAME = "MongoDB";    
+
+    public static final String ICON_BASE = "de/bfg9000/mongonb/ui/core/images/MongoDB-logo-16.png";
+    public static final String NAME = "MongoDB";
 
     private static final ResourceBundle bundle = NbBundle.getBundle(MongoServiceNode.class);
-    
+
     public MongoServiceNode() {
         super(Children.create(new ConnectionNodeFactory(), true));
-        
+
         setName(bundle.getString("MongoServiceNode.Name"));
         setIconBaseWithExtension(ICON_BASE);
     }
-    
+
     @Override
     public Action[] getActions(boolean context) {
-        return new Action[] { new CreateConnectionAction() };        
+        final List<Action> actions = new LinkedList<Action>();
+        actions.add(new CreateConnectionAction());
+        actions.addAll(Arrays.asList(super.getActions(context)));
+        return actions.toArray(new Action[actions.size()]);
     }
-    
+
 }
